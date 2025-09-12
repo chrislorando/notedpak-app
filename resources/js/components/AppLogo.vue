04c03bcf1d3bf7d5fba79f821e6167de825907e2
CML <chrismanuel.lorando@gmail.com>
2025-09-13 00:29:26 +0700

    style: Change logo

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -5,11 +5,19 @@ const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 </script>
 
 <template>
-    <div class="flex h-8 w-8 items-center justify-center rounded-xl bg-sidebar-primary text-sidebar-primary-foreground">
+    <div class="flex h-8 w-8 items-center justify-center rounded-full bg-sidebar-primary p-1 text-sidebar-primary-foreground">
         <AppLogoIcon class="h-5 w-5 fill-current text-green-400 dark:text-green-400" />
     </div>
 
-    <div class="ml-1 grid flex-1 text-left text-sm">
+    <!-- <div class="flex h-10 w-10 items-center justify-center rounded-xl">
+        <AppLogoIcon />
+    </div> -->
+
+    <!-- <div class="ml-1 grid flex-1 text-left text-sm">
         <span class="mb-0.5 truncate leading-tight font-semibold">{{ appName }}</span>
+    </div> -->
+
+    <div class="ml-1 grid flex-1 text-left text-sm">
+        <span class="truncate leading-tight font-semibold">{{ appName }}</span>
     </div>
 </template>
