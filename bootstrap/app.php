7f397a423eeb24d6b8cf7e855e522ffc7458e582
CML <chrismanuel.lorando@gmail.com>
2026-05-27 20:10:22 +0700

    feat: add trustProxies middleware configuration in app bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
         if (env('NATIVEPHP_APP_ID')) {
